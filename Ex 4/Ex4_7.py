9574eacadd3cc2854f1397cb01ab55d6caeb6bc5
Fadi Alhelo <fadi.alhelo2003@hotmail.com>
2024-06-17 09:09:32 +0200

    Calculations for the 7-th sheet

---
+++ Ex 4/Ex4_7.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+#Nr. 21
+
+## Needed constants 
+k_B = 1.380649E-23
+T = 300                     #Roomtemp.
+R_inf = 1.097E7
+c = 3E8                     #Speed of light
+m_H = 1.66053906892E-27     #Mass Hydrogen atom
+
+
+v_0 = R_inf*((1/4) - (1/9))
+delta_v = ((2*v_0)/c)*np.sqrt((2*k_B*T*np.emath.logn(np.e, 2))/(m_H))
+
+#print(f'{v_0:.05E}')
+#print (f'{delta_v:.04E}')
